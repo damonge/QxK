1a59ccdc0b82ebd7f0992a143f40222c36540bf4
Joseph Colosimo <Joseph@Josephs-MacBook-Pro.local>
2017-08-03 13:34:32 -0400

    joseph's script to find bias

--- joseph/bias.py
+++ joseph/bias.py
@@ -13,15 +13,16 @@ import pyccl as ccl
 
 # read command line arguments
 parser = argparse.ArgumentParser(description='Calculate bias of QSOs and DLAs.')
+parser.add_argument('dir', type=str, help='Base directory containing all required files.')
 parser.add_argument('-M', type=int, help='maximum ell value, default is 1000')
-parser.add_argument('-m', help='minimum bin value')
+parser.add_argument('-m', type=int, help='minimum bin value')
 parser.add_argument('-b', type=int, help='bandpower width (25, 50, or 75); default is 50')
 parser.add_argument('--sim', help='use random simulation (instead of data)',action="store_true")
-parser.add_argument('--nsim', help='simulation number (default is random)')
-parser.add_argument('--plot', help='plot resulting 2d gaussian',action="store_true")
-parser.add_argument('-A', help='use analytical errors instead of simulated error',action="store_true")
-
+parser.add_argument('--nsim', type=int,  help='simulation number (default is random)')
+parser.add_argument('--simnum', type=int, help='number of simulations used (default is all available)')
 args = parser.parse_args()
+
+dir = args.dir
 
 if args.M == None:
     max_ell = 1000
@@ -32,14 +33,57 @@ if args.b == None:
     bsize = 50
 else:
     bsize = args.b
-    
+
+#Find redshift distribution of dlas and qsos
+
+dla = open(dir+'/data/DLA_DR12_v1.dat')
+dla_ra = []; dla_dec = []; dla_z = []; qso_z = []
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+for line in dla:
+    ra = [line.split()][0][2]
+    dec = [line.split()][0][3]
+    d = [line.split()][0][9]
+    q = [line.split()][0][4]
+    if isfloat(ra):
+        dla_ra.append(float(ra))
+        dla_dec.append(float(dec))
+        dla_z.append(float(d))
+        qso_z.append(float(q))
+
+dz=.01
+rdshift = np.arange(0, 7.2, dz)
+n_dla = stats.gaussian_kde(dla_z,bw_method=0.1)(rdshift)
+n_qso = stats.gaussian_kde(qso_z,bw_method=0.1)(rdshift)
+
+#set ccl parameters
+parameters = ccl.Parameters(Omega_c=0.27,Omega_b=0.045,h=0.69,sigma8=0.83,n_s=0.96)
+cosmo = ccl.Cosmology(parameters)
+lens = ccl.ClTracerCMBLensing(cosmo)
+
+#use ccl to predict cls
+bias_qso = np.ones(rdshift.size)
+bias_dla = np.ones(rdshift.size)
+
+source_dla = ccl.ClTracerNumberCounts(cosmo, False, False, z=rdshift, n=n_dla, bias= bias_dla)
+theory_dla = ccl.angular_cl(cosmo,lens,source_dla,ell_arr,l_limber=-1)
+
+source_qso = ccl.ClTracerNumberCounts(cosmo, False, False, z=rdshift, n=n_qso, bias= bias_qso)
+theory_qso = ccl.angular_cl(cosmo,lens,source_qso,ell_arr,l_limber=-1)
+
+
 #import precalculated power spectra
 
-
-theory_dla = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/theory/theory_dla_'+str(bsize)+'.pkl','rb'))
-theory_qso = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/theory/theory_qso_'+str(bsize)+'.pkl','rb'))
-
-nsim = int(len(os.listdir('/Users/Joseph/Brookhaven/Research/pickled/sim_data'+str(bsize)+'/'))/2)
+if args.nsim == None:
+    nsim = int(len(os.listdir(dir+'/pickled/sim_data'+str(bsize)+'/'))/2)
+else:
+    nsim = args.nsim
 
 b = nmt.NmtBin(2048,nlb= bsize)
 ell_arr=b.get_effective_ells()
@@ -49,50 +93,27 @@ max_ell_bin = (np.abs(ell_arr-max_ell)).argmin()
 ells = ell_arr[min_ell_bin:max_ell_bin]
 
 if args.sim:
-    if args.nsim == None:
+    if args.simnum == None:
         N = np.random.randint(nsim)
     else:
-        N = args.nsim
+        N = args.simnum
     print('simulation number:',N)
-    data_qso = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/sim_data'+str(bsize)+'/cps_qso_'+str(N).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0][min_ell_bin:max_ell_bin]
-    data_dla = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/sim_data'+str(bsize)+'/cps_dla_'+str(N).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0][min_ell_bin:max_ell_bin]   
-else:    
-    data_qso = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/cps/cps_qso_'+str(bsize)+'.pkl','rb'))[0]
-    data_dla = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/cps/cps_dla_'+str(bsize)+'.pkl','rb'))[0]
+    data_qso = pickle.load(open(dir+'/pickled/sim_data'+str(bsize)+'/cps_qso_'+str(N).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0][min_ell_bin:max_ell_bin]
+    data_dla = pickle.load(open(dir+'/pickled/sim_data'+str(bsize)+'/cps_dla_'+str(N).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0][min_ell_bin:max_ell_bin]
+else:
+    data_qso = pickle.load(open(dir+'/pickled/cps/cps_qso_'+str(bsize)+'.pkl','rb'))[0]
+    data_dla = pickle.load(open(dir+'/pickled/cps/cps_dla_'+str(bsize)+'.pkl','rb'))[0]
 
-#Calculate error of Cls using simulations    
-sim_qso = np.zeros((len(ell_arr),nsim))
-sim_dla = np.zeros((len(ell_arr),nsim))
-for i in range(nsim):
-    sim_qso[:,i] = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/sim_data'+str(bsize)+'/cps_qso_'+str(i).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0]
-    sim_dla[:,i] = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/sim_data'+str(bsize)+'/cps_dla_'+str(i).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0]
-
-#error_qso = np.std(sim_qso,axis=1)
-#error_dla = np.std(sim_dla,axis=1)
 
 #calculate full inverse covariance matrix for all ell bins
 
-if args.A: 
-    #Approximation using errors calculated analytically (still uses l+1 correlation from sims)
-    #load masks and calculate fraction of the sky covered by them
-    qso_mask = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/masks/mask_qso_apo.pkl','rb'))
-    dla_mask = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/masks/mask_dla_apo.pkl','rb'))
-    f_qso = sum(qso_mask)/len(qso_mask)
-    f_dla = sum(dla_mask)/len(dla_mask)
-    #load power spectra
-    cps_qso = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/cps/cps_qso_'+str(bsize)+'.pkl','rb'))[0]
-    cps_dla = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/cps/cps_dla_'+str(bsize)+'.pkl','rb'))[0]
-    ps_dd = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/power_spectra/ps_dd_'+str(bsize)+'.pkl','rb'))[0]
-    ps_qq = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/power_spectra/ps_qq_'+str(bsize)+'.pkl','rb'))[0]
-    ps_kk = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/power_spectra/ps_kk_'+str(bsize)+'.pkl','rb'))[0]
-    cps_dq = pickle.load(open('/Users/Joseph/Brookhaven/Research/pickled/cps/cps_dla_qso_'+str(bsize)+'.pkl','rb'))[0]
-    #Calculate Error
-    average_qsoXqso = (np.array(abs(ps_kk*ps_qq+cps_qso**2)/(bsize*(2*ell_arr+1)*f_qso)))
-    average_dlaXdla = (np.array(abs(ps_kk*ps_dd+cps_dla**2)/(bsize*(2*ell_arr+1)*f_dla)))
-    average_qsoXdla = (np.array(abs(ps_kk*cps_dq+cps_qso*cps_dla)/(bsize*(2*ell_arr+1)*f_dla)))
-    diagP2 = np.zeros(2*len(ell_arr)-2)
-
 else:
+    #Calculate error of Cls using simulations
+    sim_qso = np.zeros((len(ell_arr),nsim))
+    sim_dla = np.zeros((len(ell_arr),nsim))
+    for i in range(nsim):
+        sim_qso[:,i] = pickle.load(open(dir+'/pickled/sim_data'+str(bsize)+'/cps_qso_'+str(i).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0]
+        sim_dla[:,i] = pickle.load(open(dir+'/pickled/sim_data'+str(bsize)+'/cps_dla_'+str(i).zfill(2)+'_'+str(bsize)+'.pkl','rb'))[0]
     average_qsoXqso = np.average(sim_qso**2,axis=1)
     average_dlaXdla = np.average(sim_dla**2,axis=1)
     average_qsoXdla = abs(np.average((sim_qso)*(sim_dla),axis=1))
@@ -127,12 +148,12 @@ Cov[2:,:len(Cov)-2] += CovP2
 Cov[:len(Cov)-2,2:] += CovP2
 
 iCov = scipy.linalg.inv(Cov)
-    
+
 if args.m == None:
     min_ell_bin = 0
 else:
     min_ell_bin = int(args.m)
-    
+
 max_ell_bin = (np.abs(ell_arr-max_ell)).argmin()
 ells = ell_arr[min_ell_bin:max_ell_bin]
 
@@ -147,7 +168,7 @@ theory_d[1::2] = theory_dla[min_ell_bin:max_ell_bin]
 
 data = np.zeros(2*len(ells))
 data[::2] = data_qso[min_ell_bin:max_ell_bin]
-data[1::2] = data_dla[min_ell_bin:max_ell_bin]                
+data[1::2] = data_dla[min_ell_bin:max_ell_bin]
 
 def loglike(x):
     delta = x[0]*theory_q + x[1]*theory_d - data
@@ -161,7 +182,7 @@ def negloglike(x):
 dof = 2*len(ells)
 chi2 = -2*loglike([b_qso, b_dla])
 prob = scipy.stats.chi2.cdf(chi2,dof)
-    
+
 
 iCovB = np.zeros((2,2))
 iCovB[0,0] = np.dot(theory_q,np.dot(iCovariance,theory_q))
@@ -173,7 +194,7 @@ CovB = scipy.linalg.inv(iCovB)
 qso_error = np.sqrt(CovB[0,0])
 dla_error = np.sqrt(CovB[1,1])
 
-        
+
 
 print('\n','b_qso:',str(np.around(b_qso,2))+' ± '+str(np.around(qso_error,2)),'\n','b_dla:',str(np.around(b_dla,2))+' ± '+str(np.around(dla_error,2)),'\n','dof:  ',dof,'\n','chi2: ',np.around(chi2,2),'\n','prob: ',np.around(prob,2),'\n')
 
@@ -188,7 +209,7 @@ if args.plot:
 
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    plt.imshow(B,interpolation='nearest')  
+    plt.imshow(B,interpolation='nearest')
     ax.set_yticklabels(np.append('',np.arange(np.round(b_qso)-3,np.round(b_qso)+3)))
     ax.set_xticklabels(np.append('',np.arange(np.round(b_dla)-3,np.round(b_dla)+3)))
     plt.colorbar()
@@ -196,31 +217,3 @@ if args.plot:
     plt.ylabel('b_qso')
     plt.show()
     plt.close()
-    
-"""if args.plot:
-    
-    def gauss(x):
-        b = np.matrix([b_qso,b_dla])
-        logl = np.dot(np.square(b-x),np.dot(iCovB,np.square(b.transpose()-x.transpose())))
-        return np.exp(-logl)
-
-    b_x = np.arange(np.round(b_qso*2)/2-3,np.round(b_qso*2)/2+3,0.01)
-    b_y = np.arange(np.round(b_dla*2)/2-3,np.round(b_dla*2)/2+3,0.01)
-    l = len(b_x)
-    B = np.zeros((l,l))
-    for i in range(l):
-        for j in range(l):
-            x = np.matrix([b_x[i],b_y[j]])
-            B[i,j] = gauss(x)
-    
-    B = np.flipud(B)
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    plt.imshow(B,interpolation='nearest')  
-    ax.set_yticklabels(np.append('',np.arange(np.round(b_qso*2)/2+3,np.round(b_qso*2)/2-3,-1)))
-    ax.set_xticklabels(np.append('',np.arange(np.round(b_dla*2)/2-3,np.round(b_dla*2)/2+3,1)))
-    plt.colorbar()
-    plt.xlabel('b_dla')
-    plt.ylabel('b_qso')
-    plt.show()
-    plt.close()"""
